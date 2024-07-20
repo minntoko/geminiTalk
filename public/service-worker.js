75955f10a8ebf61612f8a929e9c04d1737ca8875
minntoko <minntoko0822@gmail.com>
2024-07-20 22:40:19 +0900

    add: PWAに対応

---
+++ public/service-worker.js
@@ -0,0 +1,61 @@
+// src/sw.ts
+
+const CACHE_NAME = 'gemini-talk-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/assets/index-*.js',
+  '/assets/index-*.css',
+  '/images/icon-512.png',
+  '/images/icon.png',
+  '/images/compass.svg',
+  '/images/google-gemini.svg',
+  '/images/light.svg',
+  '/images/pen.svg',
+  '/images/plus.svg',
+  '/images/terminal.svg',
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      if (response) {
+        return response;
+      }
+      const fetchRequest = event.request.clone();
+      return fetch(fetchRequest).then((networkResponse) => {
+        if (!networkResponse || networkResponse.status !== 200 || networkResponse.type !== 'basic') {
+          return networkResponse;
+        }
+        const responseToCache = networkResponse.clone();
+        caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, responseToCache);
+        });
+        return networkResponse;
+      });
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
--- src/main.tsx
+++ src/main.tsx
@@ -11,3 +11,9 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     </BrowserRouter>
   </React.StrictMode>
 );
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js')
+  });
+}
