ff64d248d45dd8c68ddfb651c1f9ad110b691b43
minntoko <minntoko0822@gmail.com>
2024-08-24 20:33:53 +0900

    fix: APIキーを登録して即時反映されるように変更

--- src/pages/Setting.tsx
+++ src/pages/Setting.tsx
@@ -61,7 +61,7 @@ const Setting = () => {
   const registerAPIKey = () => {
     inputAPIKey.trim();
     if (!inputAPIKey) return;
-    setApiKey(apiKey.slice(0, 6) + apiKey.slice(6).replace(/./g, "."));
+    setApiKey(inputAPIKey.slice(0, 6) + inputAPIKey.slice(6).replace(/./g, "."));
     setAPIKey(inputAPIKey);
     setInputAPIKey("");
     setIsRegister(true);
@@ -73,7 +73,7 @@ const Setting = () => {
       setApiKey(apiKey.slice(0, 6) + apiKey.slice(6).replace(/./g, "."));
       setIsRegister(true);
     }
-  }, []);
+  }, [apiKey]);
 
   const getAddModelList = () => getModelList()?.filter((model) => model.name !== "gemini-1.5-flash" && model.name !== "gemini-1.5-pro" && model.name !== "gemini-1.0-pro");
 
@@ -102,7 +102,7 @@ const Setting = () => {
             <SButton onClick={registerAPIKey}>登録</SButton>
             <SApiBox>
               <SSvg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" width={20}><path d="M336 352c97.2 0 176-78.8 176-176S433.2 0 336 0S160 78.8 160 176c0 18.7 2.9 36.8 8.3 53.7L7 391c-4.5 4.5-7 10.6-7 17l0 80c0 13.3 10.7 24 24 24l80 0c13.3 0 24-10.7 24-24l0-40 40 0c13.3 0 24-10.7 24-24l0-40 40 0c6.4 0 12.5-2.5 17-7l33.3-33.3c16.9 5.4 35 8.3 53.7 8.3zM376 96a40 40 0 1 1 0 80 40 40 0 1 1 0-80z"/></SSvg>
-              <p>{apiKey}</p>
+              <p>{isRegister ? apiKey : "APIキーが登録されていません"}</p>
             </SApiBox>
             <SCloudModels isRegister={isRegister}>
               <SCloudModel
